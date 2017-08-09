0814acda305beae530fa8aa7a97dc3c25c71eca1
houlong.yang <houlong.yang@ele.me>
2017-08-09 18:52:57 +0800

    init project

---
+++ src/main/java/com/hialan/dm/DirectMessageApplication.java
@@ -0,0 +1,12 @@
+package com.hialan.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectMessageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hialan/dm/DirectMessageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hialan.dm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DirectMessageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
